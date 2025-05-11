1fbcd9db9e6a50d38d2edf910d57a1fa44fcf66e
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-11 19:21:08 +0100

    feat: add authentication routes and integrate with server

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
