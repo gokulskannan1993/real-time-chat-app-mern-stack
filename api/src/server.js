ab3b84d4c8243304277c470bda87975ba6a7145d
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-11 19:52:33 +0100

    feat: add MongoDB connection utility and integrate with server

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
