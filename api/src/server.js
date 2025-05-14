3b34c7e3e45a5c1ad22630578b0752c0bec8201c
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-14 20:07:03 +0100

    feat: add cookie parser middleware for enhanced request handling

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,6 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
