6e33e2918a19cc0a55455746b0d8d505323ccd7b
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-18 12:05:21 +0100

    feat: add CORS middleware and update dependencies

--- api/src/server.js
+++ api/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 
@@ -14,10 +15,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
-
 
 app.listen(PORT, () => {
   connectDB();
