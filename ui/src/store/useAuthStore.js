4417c2f72dce6ceec2d11a3d89248554cf32673a
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-18 11:32:52 +0100

    feat: implement authentication store using zustand

---
+++ ui/src/store/useAuthStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+      set({ authUser: response.data });
+    } catch (error) {
+      console.error("Error checking auth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
