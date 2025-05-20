53016d389322fa1401d58bce89a5f8edf833a593
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-20 22:08:56 +0100

    feat: implement theme selection and preview in SettingsPage, add theme store and constants

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -9,9 +9,11 @@ import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -28,7 +30,7 @@ function App() {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
